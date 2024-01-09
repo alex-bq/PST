2bfd1ec80d9835d24eb18cb3bbf2390289ec4d2f
alex-bq <ale.barrientos@duocuc.cl>
2024-01-09 17:31:44 -0300

    CONEXION CON BD SQL SERVER PUM

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,17 @@ use App\Http\Controllers\ListadoController;
 //    return view('index');
 // });
 Route::get('/', [ListadoController::class, 'index']);
+
+Route::get('/test-database-connection', function () {
+    try {
+        $pdo = new PDO(
+            "sqlsrv:Server=" . config('database.connections.sqlsrv.host') . "," . config('database.connections.sqlsrv.port') . ";Database=" . config('database.connections.sqlsrv.database'),
+            config('database.connections.sqlsrv.username'),
+            config('database.connections.sqlsrv.password')
+        );
+
+        dd('Conexión exitosa');
+    } catch (PDOException $e) {
+        dd("Error de conexión: " . $e->getMessage());
+    }
+});
